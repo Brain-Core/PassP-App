8bb4093bca4482c18fe7a8e59ba45275619844ce
funcoder <grace.devolop@gmail.com>
2020-10-24 11:51:27 +0200

    Route Protection

---
+++ middleware/protectedRoute.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const protectRoute = (req, res, next) => {
+    const token = req.header['app-token'];
+    if(!token) res.status(400).json({msg: 'no token' })
+
+    try{
+        const testToken = jwt.verify(token, config.get('secret_key'));
+        req.user = testToken;
+        next();
+    }
+    catch(e){
+        res.json({msg: e});
+    }
+}
+
+module.exports = protectRoute;
