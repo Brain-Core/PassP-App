eeffbb5b4d22199e47fa04d8dd95b282a06a86ca
bvenceslas <bvenceslas@gmail.com>
2020-10-25 11:59:00 +0200

    Create Client Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 
 import './App.css';
 import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Profile from './components/Profile'
 import NavBar from './components/NavBar';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Login />
+      <Router>
+        <NavBar />
+        <Route exact path='/' component={Login} />
+        <Route exact path='/SignUp' component={SignUp} />
+        <Route exact path='/Profile' component={Profile} />
+      </Router>
+      
     </div>
   );
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-
+import {Link} from 'react-router-dom'
 class NavBar extends Component {
     state = {  }
     render() { 
         return ( 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Profile complete</a>
+        <Link class="navbar-brand" to="#">ProfileX</Link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Profile <span class="sr-only">(current)</span></a>
+              <Link class="nav-link" to="/Profile">Profile <span class="sr-only">(current)</span></Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Login</a>
+              <Link class="nav-link" to="/">Login</Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Sign Up</a>
+              <Link class="nav-link" to="/SignUp">Sign Up</Link>
             </li>
           </ul>
           <span class="navbar-text">
